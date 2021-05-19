b721d10a178c0c306eac9695fa736d306fa852da
mai174143 <83526796+mai174143@users.noreply.github.com>
2021-05-19 09:47:36 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 import { dog1, dog2 } from "./data/dogData";
 
+on:
+  schedule:
+    - cron: '*/5 * * * *' # will run every 5 minutes
+
+jobs:
+  ping_url:
+    runs-on: ubuntu-latest
+    name: Ping URL
+    steps:
+      - name: Check the URL
+        id: ping
+        uses: soul-wish/ping-url-javascript-action@v1.1
+        with:
+          url: 'https://google.com'
+
+
 console.log("---------");
 dog1.info();
 console.log("---------");
